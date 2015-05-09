9e0dc95d191232eba81e28b3c143bc14bf52f33e
Samiksha Ande <samiksha@thoughtworks.com>
2015-05-09 14:01:16 +0530

    #101 - Digi/Kavita/samiksha - using request matchers in routes and removing welcome controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :books
-  # get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'books#index'
+
+  get 'books' => 'books#index', as: 'books'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
