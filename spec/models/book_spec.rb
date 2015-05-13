8924bc835dfad1d227907d1af7c301f6161cd58a
vagrant <vagrant@ubuntu-14.04-amd64-vbox>
2015-05-13 13:18:30 +0000

    [Misc] - Kavita - Fixing specs as isbn need to be uniq and not null

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe Book do
   context '#search' do
     it 'should give empty array when no book present with title having search parameter' do
-      FactoryGirl.create(:book, title:'Malgudi days')
-      FactoryGirl.create(:book, title:'Java programming')
+      FactoryGirl.create(:book, isbn:'111', title:'Malgudi days')
+      FactoryGirl.create(:book, isbn:'112', title:'Java programming')
       expect(Book.search('the')).to eq([])
     end
 
     it 'should give book with title having search parameter' do
-      book1 = FactoryGirl.create(:book, title:'Malgudi days')
-      book2 = FactoryGirl.create(:book, title:'Java programming')
-      book3 = FactoryGirl.create(:book, title:'Ruby programming')
+      book1 = FactoryGirl.create(:book, isbn:'111', title:'Malgudi days')
+      book2 = FactoryGirl.create(:book, isbn:'112', title:'Java programming')
+      book3 = FactoryGirl.create(:book, isbn:'113', title:'Ruby programming')
       expect(Book.search('programming')).to match_array([book2,book3])
     end
   end
@@ -19,9 +19,9 @@ describe Book do
   context '#order_by_title' do
     it 'should give book with title in ascending order' do
 
-      book1 = FactoryGirl.create(:book, title:'Malgudi days')
-      book2 = FactoryGirl.create(:book, title:'Java programming')
-      book3 = FactoryGirl.create(:book, title:'Ruby programming')
+      book1 = FactoryGirl.create(:book, isbn:'111', title:'Malgudi days')
+      book2 = FactoryGirl.create(:book, isbn:'112', title:'Java programming')
+      book3 = FactoryGirl.create(:book, isbn:'113', title:'Ruby programming')
 
       expect(Book.order_by('title')).to eq([book2,book1,book3])
     end
