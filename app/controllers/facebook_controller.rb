9ebd061a346bbb83a457680ca8ae22996271ec54
Samiksha Ande <samiksha@thoughtworks.com>
2015-05-14 18:59:32 +0530

    #1003 - Kashish - refactored facebook controller.

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,26 +1,38 @@
 class FacebookController < Devise::OmniauthCallbacksController
   def facebook
     auth = request.env["omniauth.auth"]
-    @user = User.from_omniauth(auth)
+    members = get_user_details(auth)
+    authorise_user(auth, members)
+  end
 
+  def authorise_user(auth, members)
+    if members.empty?
+      sign_out_and_redirect
+    else
+      sign_in_to_library(auth, members)
+    end
+  end
+
+  def sign_in_to_library(auth, members)
+    first = members.select { |member| member['id'] == auth['uid'] }.first
+    @user.role = first['administrator'] ? User::Role::ADMIN : User::Role::INTERN
+    @user.name = first['name']
+    @user.save
+    sign_in_and_redirect @user
+  end
+
+  def sign_out_and_redirect
+    @user.destroy
+    sign_out @user
+    flash[:sign_in_error] = 'Oops! You are not a part of STEP';
+    redirect_to '/user/sign_in'
+  end
+
+  def get_user_details(auth)
+    @user = User.from_omniauth(auth)
     members_limit = 1000
     access_token = auth['credentials']['token']
-
     @graph = Koala::Facebook::API.new(access_token)
-    profile = @graph.get_object('me')
-    tw_step_group_members = @graph.get_connections(Rails.application.config.group_id, 'members', {limit: members_limit})
-    if tw_step_group_members.empty?
-      @user.destroy
-      sign_out @user
-      flash[:sign_in_error] = 'Oops! You are not a part of STEP';
-      redirect_to '/user/sign_in'
-    else
-      first = tw_step_group_members.select { |member| member['id'] == auth['uid'] }.first
-      @user.role = first['administrator'] ? User::Role::ADMIN : User::Role::INTERN
-      @user.name = first['name']
-
-      @user.save
-      sign_in_and_redirect @user
-    end
+    @graph.get_connections(Rails.application.config.group_id, 'members', {limit: members_limit})
   end
 end
