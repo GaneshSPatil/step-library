c980c41819c5aec5189cf19bda0d23c9580e91ce
Samiksha Ande <samiksha@thoughtworks.com>
2015-05-14 18:24:06 +0530

    #1003 - Kashish/Samiksha - showing error message if user is not present in STEP Facebook group.

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -9,9 +9,11 @@ class FacebookController < Devise::OmniauthCallbacksController
     @graph = Koala::Facebook::API.new(access_token)
     profile = @graph.get_object('me')
     tw_step_group_members = @graph.get_connections(Rails.application.config.group_id, 'members', {limit: members_limit})
-
     if tw_step_group_members.empty?
-      sign_out_and_redirect @user
+      @user.destroy
+      sign_out @user
+      flash[:sign_in_error] = 'Oops! You are not a part of STEP';
+      redirect_to '/user/sign_in'
     else
       first = tw_step_group_members.select { |member| member['id'] == auth['uid'] }.first
       @user.role = first['administrator'] ? User::Role::ADMIN : User::Role::INTERN
