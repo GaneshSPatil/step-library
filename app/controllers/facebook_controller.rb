3e08e1b304bff33a45cdd4ce4af8dee9f6feb97f
Kashishm <kashishmunjal64@gmail.com>
2015-05-15 13:08:44 +0530

    #1003 - Kashish/Samiksha - refactoring login controller

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,7 +1,7 @@
 class FacebookController < Devise::OmniauthCallbacksController
   def facebook
     auth = request.env["omniauth.auth"]
-    members = get_user_details(auth)
+    members = get_group_members(auth)
     authorise_user(auth, members)
   end
 
@@ -9,27 +9,16 @@ class FacebookController < Devise::OmniauthCallbacksController
     if members.empty?
       sign_out_and_redirect
     else
-      sign_in_to_library(auth, members)
+      sign_in_and_redirect User.from_omniauth(auth, members)
     end
   end
 
-  def sign_in_to_library(auth, members)
-    first = members.select { |member| member['id'] == auth['uid'] }.first
-    @user.role = first['administrator'] ? User::Role::ADMIN : User::Role::INTERN
-    @user.name = first['name']
-    @user.save
-    sign_in_and_redirect @user
-  end
-
   def sign_out_and_redirect
-    @user.destroy
-    sign_out @user
     flash[:sign_in_error] = ['Oops! You are not a part of STEP', 'Please check if you are logged in on facebook as a valid STEP user']
     redirect_to user_session_path
   end
 
-  def get_user_details(auth)
-    @user = User.from_omniauth(auth)
+  def get_group_members(auth)
     members_limit = 1000
     access_token = auth['credentials']['token']
     @graph = Koala::Facebook::API.new(access_token)
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,14 @@ class User < ActiveRecord::Base
     INTERN = 'Intern'
   end
 
-  def self.from_omniauth(auth)
+  def self.from_omniauth(auth, members)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      first = members.select { |member| member['id'] == auth['uid'] }.first
+      user.role = first['administrator'] ? User::Role::ADMIN : User::Role::INTERN
+      user.name = first['name']
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
-      user.role = auth.role
-      user.name = auth.name
     end
   end
 
