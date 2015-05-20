054e56fc1b005ce63295218c86611001f2151422
Prajaktakudale <prajaktagpk@gmail.com>
2015-05-20 12:10:10 +0530

    #103 - Prajakta/Digvijay - Redesigning of add new book page and UI fixed.

--- app/assets/javascripts/manage_books.js
+++ app/assets/javascripts/manage_books.js
@@ -1,47 +1,51 @@
-var showBookForm = function () {
-    var isUploadSingle = jQuery('#uplaod_single_book').is(':checked');
-    if (isUploadSingle) {
-        jQuery("#add_book_modal").show();
-    }
-    else
-        jQuery("#add_book_modal").hide();
+var hideAlert = function(){
+    jQuery('#no_book_error').hide();
+    jQuery('#no_book_error_message').text("");
 };
 
-var extractBookDetails = function (response, isbn) {
-    jQuery('.alert').hide();
-    if (response.totalItems === 0) {
-        jQuery('#no_book_error_message').text("Invalid ISBN :- " + isbn);
-        jQuery('#no_book_error').show();
-        return;
-    }
-    var volumeInfo = response.items[0].volumeInfo;
-    var title = volumeInfo.title;
-    var authors = volumeInfo.authors;
-    var image_links = volumeInfo.imageLinks;
+var showNoBooksFoundError =function(isbn) {
+    jQuery('#no_book_error_message').text("Invalid ISBN : \'" + isbn + "\'");
+    jQuery('#no_book_error').show();
+};
+
+var getImageThumbnailURL = function(image_links) {
     var thumbnail = image_links && image_links.thumbnail;
-    thumbnail = thumbnail || '/assets/default-book.png';
+    return thumbnail || '/assets/default-book.png';
+};
+
+var setBookDetails = function(response, isbn) {
+    var volumeInfo = response.items[0].volumeInfo;
+    var image_links = volumeInfo.imageLinks;
+    var thumbnail = getImageThumbnailURL(image_links);
+
     jQuery('#isbn_confirm').val(isbn);
-    jQuery('#title_confirm').val(title);
-    jQuery('#author_confirm').val(authors[0]);
+    jQuery('#title_confirm').val(volumeInfo.title);
+    jQuery('#author_confirm').val(volumeInfo.authors);
     if (image_links)
         jQuery('#image_confirm').val(thumbnail);
     jQuery('#book_image_confirm').attr('src', thumbnail);
     jQuery('#confirm_book_modal').show();
-    jQuery('#add_book_modal').hide();
-    jQuery('#add_book_controls').hide();
+};
+
+var processBookDetails = function (response, isbn) {
+    jQuery('.alert').hide();
+    if (response.totalItems === 0) {
+        showNoBooksFoundError(isbn);
+        rejectNewBook();
+        return;
+    }
+    setBookDetails(response, isbn);
 };
 
 var fetchBookDetails = function () {
     var isbn = jQuery('#isbn_fetch').val().trim();
-    var googleApiUrl = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn
+    var googleApiUrl = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn;
     jQuery.get(googleApiUrl, function (response) {
-        extractBookDetails(response, isbn)
+        processBookDetails(response, isbn)
     });
 };
 
 var rejectNewBook = function () {
-    jQuery('#uplaod_single_book').prop('checked', false);
     jQuery('#confirm_book_modal').hide();
-    jQuery('#add_book_modal').hide();
-    jQuery('#add_book_controls').show();
+    jQuery('#add_book_modal').show();
 };
