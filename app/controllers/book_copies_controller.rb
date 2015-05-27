67176c48908e67012111d8efd83f2e4f20b9a13b
Shweta Dongare <shwetado@thoughtworks.com>
2015-05-27 14:22:49 +0530

    #133 - Shweta/Pallavi - refactoring

--- app/controllers/book_copies_controller.rb
+++ app/controllers/book_copies_controller.rb
@@ -3,7 +3,7 @@ class BookCopiesController < ApplicationController
   def logs
     book_copy_id = params[:id]
     @book         = BookCopy.where(id: book_copy_id).first.book
-    @book_copies = BookCopy.where(book_id: @book.id)
+    @book_copies = @book.book_copies
     @records = Record.includes(:user).all.where("records.book_copy_id == #{book_copy_id}")
 
     respond_to do |format|
