c225fa6933ea6f66254c3d043c9c4bb48f345066
Shital Mane <shitalmane639@gmail.com>
2015-06-04 17:00:42 +0530

    #000 - Shweta/Kashish - added More »/« Less text

--- app/assets/javascripts/show_books.js
+++ app/assets/javascripts/show_books.js
@@ -1,8 +1,8 @@
 var readMoreCallBack = function () {
     var charactersToShow = 400;  // How many characters are shown by default
-    var ellipsestext = "...";
-    var moretext = "show more";
-    var lesstext = "show less";
+    var ellipsesText = "...";
+    var moreText = "More »";
+    var lessText = "« Less";
 
 
     jQuery('#description p').each(function() {
@@ -13,9 +13,9 @@ var readMoreCallBack = function () {
             var contentToShow = content.substr(0, charactersToShow);
             var contentToHide = content.substr(charactersToShow, content.length - charactersToShow);
 
-            var html = contentToShow + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span>' +
+            var html = contentToShow + '<span class="moreellipses">' + ellipsesText+ '&nbsp;</span>' +
                        '<span class="morecontent"><span>' + contentToHide + '</span>&nbsp;&nbsp;' +
-                       '<a href="" class="morelink">' + moretext + '</a></span>';
+                       '<a href="" class="morelink">' + moreText + '</a></span>';
 
             jQuery(this).html(html);
         }
@@ -25,10 +25,10 @@ var readMoreCallBack = function () {
     jQuery(".morelink").click(function(){
         if(jQuery(this).hasClass("less")) {
             jQuery(this).removeClass("less");
-            jQuery(this).html(moretext);
+            jQuery(this).html(moreText);
         } else {
             jQuery(this).addClass("less");
-            jQuery(this).html(lesstext);
+            jQuery(this).html(lessText);
         }
         jQuery(this).parent().prev().toggle();
         jQuery(this).prev().toggle();
